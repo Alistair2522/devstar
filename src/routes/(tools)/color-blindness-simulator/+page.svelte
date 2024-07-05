5a95d065020abd7eb46c167767e4144410b86b66
Alistair Dsouza <alistair2522@gmail.com>
2024-07-05 22:58:52 +0530

    added Algorithm again

--- src/routes/(tools)/color-blindness-simulator/+page.svelte
+++ src/routes/(tools)/color-blindness-simulator/+page.svelte
@@ -1,12 +1,20 @@
 <script>
    import { onMount } from 'svelte';
 
-   let filters = [
-    'Protanopia', 'Tritanopia',
-    'Deuteranopia', 'Achromatopsia',
-    'Protanomaly', 'Tritanomaly',
-    'Deuteranomaly', 'Achromatomaly'
-  ];
+  let imageUrl = '';
+	let imageFile;
+	let fileName = '';
+	let canvasOriginal;
+	let ctxOriginal;
+	let canvasFiltered;
+	let ctxFiltered;
+	let filters = [
+	  'Protanopia', 'Tritanopia',
+	  'Deuteranopia', 'Achromatopsia',
+	  'Protanomaly', 'Tritanomaly',
+	  'Deuteranomaly', 'Achromatomaly'
+	];
+
 
   const filterMatrices = {
     normal: {
@@ -108,6 +116,33 @@
     };
   }
 
+  function drawFilteredImage(filter) {
+	  const img = new Image();
+	  img.src = imageUrl;
+	  img.onload = () => {
+		canvasFiltered.width = img.width;
+		canvasFiltered.height = img.height;
+		ctxFiltered.clearRect(0, 0, canvasFiltered.width, canvasFiltered.height);
+		ctxFiltered.drawImage(img, 0, 0);
+		
+		const imageData = ctxFiltered.getImageData(0, 0, canvasFiltered.width, canvasFiltered.height);
+		const data = imageData.data;
+		const matrix = filterMatrices[filter];
+	
+		for (let i = 0; i < data.length; i += 4) {
+		  const r = data[i];
+		  const g = data[i + 1];
+		  const b = data[i + 2];
+		  data[i] = r * matrix.R[0] + g * matrix.R[1] + b * matrix.R[2];
+		  data[i + 1] = r * matrix.G[0] + g * matrix.G[1] + b * matrix.G[2];
+		  data[i + 2] = r * matrix.B[0] + g * matrix.B[1] + b * matrix.B[2];
+		}
+  
+		ctxFiltered.putImageData(imageData, 0, 0);
+	  };
+	}
+
+
     function saveImage() {
       const link = document.createElement('a');
       link.download = 'filtered_image.png';
@@ -160,4 +195,5 @@
   </div>
   
   <style>
+  </style>
 
